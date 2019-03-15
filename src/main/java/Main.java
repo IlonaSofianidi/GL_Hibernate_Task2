a17a02f80e348e61482d69e4ab88fab76a0e105b
IlonaSofianidi <sofianidi.ilona@gmail.com>
2019-03-15 19:21:09 +0200

    Change main method.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,24 @@
+import dao.daoImpl.DepartmentDao;
+import dao.daoImpl.WorkerDao;
+import entity.Department;
+import entity.Worker;
+import entity.WorkerAvailability;
 
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        DepartmentDao departmentDao = new DepartmentDao();
+        departmentDao.openSessionTransaction();
+        List<Department> departmentsByStatus = departmentDao.getDepartmentByStatusUsingCriteria(true);
+        departmentDao.getCurrentSession().close();
+        System.out.println(Arrays.toString(departmentsByStatus.toArray()));
+        WorkerDao workerDao = new WorkerDao();
+        workerDao.openSessionTransaction();
+        List<Worker> workersByDepartmentIdAndAvailability = workerDao.getWorkersByDepartmentIdAndAvailability(1, WorkerAvailability.PART_TIME);
+        System.out.println(Arrays.toString(workersByDepartmentIdAndAvailability.toArray()));
+        workerDao.getCurrentSession().close();
 
     }
 }
