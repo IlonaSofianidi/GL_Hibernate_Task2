12eadd3abbac0f03ed35d018112b4651b242d5cb
IlonaSofianidi <sofianidi.ilona@gmail.com>
2019-03-15 19:19:08 +0200

    Add method to get department by status.

--- src/main/java/dao/daoImpl/DepartmentDao.java
+++ src/main/java/dao/daoImpl/DepartmentDao.java
@@ -4,8 +4,12 @@ import dao.Dao;
 import entity.Department;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import util.HibernateUtil;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,6 +26,10 @@ public class DepartmentDao implements Dao<Department> {
 
     public Session getCurrentSession() {
         return currentSession;
+    }
+
+    public void setCurrentSession(Session session) {
+        this.currentSession = session;
     }
 
     @Override
@@ -56,5 +64,26 @@ public class DepartmentDao implements Dao<Department> {
         for (Department department : departments) {
             getCurrentSession().delete(department);
         }
+    }
+
+    //homework using hql
+    public List<Department> getDepartmentByStatus(Boolean status) {
+        Query query = currentSession.createQuery("from Department as department where department.status = :code");
+        query.setParameter("code", status);
+        return query.list();
+    }
+
+    //homework using persistence criteria
+    public List<Department> getDepartmentByStatusUsingCriteria(Boolean status) {
+        CriteriaBuilder builder = currentSession.getCriteriaBuilder();
+
+        CriteriaQuery<Department> criteria = builder.createQuery(Department.class);
+        Root<Department> root = criteria.from(Department.class);
+
+        criteria.select(root).where(builder.equal(root.get("status"), status));
+
+        Query<Department> q = currentSession.createQuery(criteria);
+        return q.getResultList();
+
     }
 }
