3cff20e99d13936fabad474411886d5f605cd238
IlonaSofianidi <sofianidi.ilona@gmail.com>
2019-03-15 19:18:14 +0200

    Add method to get workers by department id and availability.

--- src/main/java/dao/daoImpl/WorkerDao.java
+++ src/main/java/dao/daoImpl/WorkerDao.java
@@ -2,10 +2,15 @@ package dao.daoImpl;
 
 import dao.Dao;
 import entity.Worker;
+import entity.WorkerAvailability;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 import util.HibernateUtil;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 import java.util.List;
 import java.util.Optional;
 
@@ -55,5 +60,35 @@ public class WorkerDao implements Dao<Worker> {
         for (Worker worker : allWorkers) {
             getCurrentSession().delete(worker);
         }
+    }
+
+    public void setCurrentSession(Session session) {
+        this.currentSession = session;
+    }
+
+    //homework using hql
+    public List<Worker> getWorkersByDepartmentIdAndAvailability(int departmentId, WorkerAvailability availability) {
+        Query query = currentSession.createQuery("from Worker as worker where worker.department.id = :code AND worker.availability = :code2");
+        query.setParameter("code", departmentId);
+        query.setParameter("code2", availability);
+        return query.list();
+
+
+    }
+
+    //homework using persistence criteria
+    public List<Worker> getWorkersByDepartmentIdAndAvailabilityUsingCriteria(int departmentId, WorkerAvailability availability) {
+        CriteriaBuilder builder = currentSession.getCriteriaBuilder();
+
+        CriteriaQuery<Worker> criteria = builder.createQuery(Worker.class);
+        Root<Worker> root = criteria.from(Worker.class);
+
+        criteria.select(root).where(builder.and(builder.equal(root.get("department").get("id"), departmentId)),
+                (builder.equal(root.get("availability"), availability)));
+
+        Query<Worker> q = currentSession.createQuery(criteria);
+
+        return q.getResultList();
+
     }
 }
